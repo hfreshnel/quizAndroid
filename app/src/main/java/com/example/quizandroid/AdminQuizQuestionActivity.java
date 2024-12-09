36f50b743c0effea869e6c88515ef585f873b010
kksua <kanampalla.amarajeeva@groupe-esigelec.org>
2024-12-09 14:53:20 +0100

    add the files

--- app/src/main/java/com/example/quizandroid/AdminQuizQuestionActivity.java
+++ app/src/main/java/com/example/quizandroid/AdminQuizQuestionActivity.java
@@ -15,7 +15,7 @@ public class AdminQuizQuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_quiz_question);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.validerButton), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
