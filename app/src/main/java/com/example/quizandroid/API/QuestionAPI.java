96646b94e51e1b46702a6614e1462fa12f0433b1
seif0099 <seifbenhassen99@gmail.com>
2025-01-10 10:22:30 +0100

    ajout websocket

--- app/src/main/java/com/example/quizandroid/API/QuestionAPI.java
+++ app/src/main/java/com/example/quizandroid/API/QuestionAPI.java
@@ -6,13 +6,51 @@ import com.google.gson.JsonObject;
 import java.io.IOException;
 
 public class QuestionAPI {
-    private static final String BASE_URL = "https://example.com";
-    private final OkHttpClient client;
-    private final Gson gson;
+    private static final String BASE_URL = "http://192.168.1.247:8080";
+    private static OkHttpClient client = new OkHttpClient();
+    private static Gson gson = new Gson();
 
     public QuestionAPI() {
         this.client = new OkHttpClient();
         this.gson = new Gson();
+    }
+
+    /**
+     * @param id the quiz id
+     * @return all ids of questions related to the auiz
+     * @throws IOException
+     */
+    public static int[] getAllQuestionsIds(long id) throws IOException {
+//        Request request = new Request.Builder()
+//                .url(BASE_URL + "/admin/quiz/questionsids" + id)
+//                .get()
+//                .build();
+//
+//        try (Response response = client.newCall(request).execute()) {
+//            if (response.isSuccessful() && response.body() != null) {
+//                return gson.fromJson(response.body().string(), JsonObject.class);
+//            } else {
+//                throw new IOException("Failed to retrieve questions ids: " + response.message());
+//            }
+//        }
+        return new int[]{1,2,3,4,5,6};
+    }
+
+    public static JsonObject getQuestion(long id) throws IOException {
+        Request request = new Request.Builder()
+                .url(BASE_URL+"/public/questions/"+id)
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                return gson.fromJson(response.body().string(), JsonObject.class);
+            } else {
+                throw new IOException("Failed to retrieve question: " + response.message());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public JsonObject getAllQuizzes(long id) throws IOException {
---
+++ app/src/main/java/com/example/quizandroid/admin/WebSocketManager.java
@@ -0,0 +1,177 @@
+package com.example.quizandroid.admin;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+/**
+ * Classe de gestion de la connexion au socket
+ * @author William Priso and Clovis Neya
+ *
+ */
+public class WebSocketManager {
+
+    private static WebSocketManager instance;
+    private static Socket mSocket;
+    private final Map<String, Emitter.Listener> eventListeners = new HashMap<>();
+
+    private WebSocketManager() {
+        try {
+            mSocket = IO.socket("http://192.168.1.247:8081");
+            mSocket.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Permet de se connecter au websocket
+     * @return le singleton de connexion au socket
+     */
+    public static synchronized WebSocketManager getInstance() {
+        if (instance == null) {
+            instance = new WebSocketManager();
+        }
+        return instance;
+    }
+
+    /**
+     * Permet d'ecouter les evenements
+     * @param event "joinedQuiz", "quizStarted", "nextQuestion", "showStatistique", "showClassement", "showAnswer", "quizEnded"
+     * @param listener methode executée lorsque lorsque ce message est recu
+     */
+    public void on(String event, Emitter.Listener listener) {
+        if (mSocket != null ) {
+            if (!eventListeners.containsKey(event)) {
+                mSocket.on(event, listener);
+                eventListeners.put(event, listener);
+            }
+        }
+    }
+
+    /**
+     * Permet d'arreter d'ecouter un evenement
+     * @param event
+     */
+    public void off(String event) {
+        if (mSocket != null && eventListeners.containsKey(event)) {
+            mSocket.off(event, eventListeners.get(event));
+            eventListeners.remove(event);
+        }
+    }
+
+    /**
+     * Publier un evement
+     * @param event "joinQuiz", "startQuiz", "showAnswer", "nextQuestion", "showStatistique", "showClassement", "endQuiz"
+     * @param data données que l'on souhaite publier au format JSON
+     */
+    public void emit(String event, JSONObject data) {
+        if (mSocket != null && mSocket.connected()) {
+            mSocket.emit(event, data);
+        }
+    }
+
+    public void emit(String event, JsonObject data) {
+        if (mSocket != null && mSocket.connected()) {
+            mSocket.emit(event, data);
+        }
+    }
+
+    /**
+     * Se deconnecter du socket
+     */
+    public void disconnect() {
+        if (mSocket != null) {
+            for (Map.Entry<String, Emitter.Listener> entry : eventListeners.entrySet()) {
+                mSocket.off(entry.getKey(), entry.getValue());
+            }
+            mSocket.disconnect();
+        }
+    }
+
+    /**
+     * Permet d'extraire la question de la reponse du socket
+     * @param socketData Liste d'objets transmise par le socket
+     * @return l'objet question
+     */
+    public JSONObject extractQuestion(Object... socketData){
+        //Extraction des informations importantes recues par le socket
+        JSONObject data = (JSONObject) socketData[0];
+        JSONObject question;
+        try {
+            question = data.getJSONObject("data");
+            question = question.getJSONObject("data");
+            return question;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    public JsonObject extractQuestion2(Object... socketData) {
+        if (socketData.length == 0 || !(socketData[0] instanceof JSONObject)) {
+            return null;
+        }
+
+        JSONObject jsonObject = (JSONObject) socketData[0];
+        try {
+            JsonObject gsonData = JsonParser.parseString(jsonObject.toString()).getAsJsonObject();
+            JsonElement dataElement = gsonData.get("data");
+
+            if (dataElement != null && dataElement.isJsonPrimitive()) {
+                // "data" est une chaîne JSON qu'il faut parser
+                String innerDataString = dataElement.getAsString();
+                JsonObject innerDataObject = JsonParser.parseString(innerDataString).getAsJsonObject();
+                return innerDataObject.getAsJsonObject("data");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * Permet d'extraire les statistiques de la reponse du socket
+     * @param socketData Liste d'objets transmise par le socket
+     * @return l'objet statistique
+     */
+    public JSONObject extractStatistique(Object... socketData){
+        //Extraction des informations importantes recues par le socket
+        JSONObject data = (JSONObject) socketData[0];
+        try {
+            JSONObject statistiques = data.getJSONObject("data");
+            return statistiques;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Permet d'extraire le classement de la reponse du socket
+     * @param socketData Liste d'objets transmise par le socket
+     * @return l'objet classement
+     */
+    public JSONObject extractClassement(Object... socketData){
+        //Extraction des informations importantes recues par le socket
+        JSONObject data = (JSONObject) socketData[0];
+        JSONObject classement;
+        try {
+            classement = data.getJSONObject("data");
+            classement = classement.getJSONObject("data");
+            return classement;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+}
